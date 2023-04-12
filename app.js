e9b2d72e35a90b55fd806afb5a19babdd61d9c2f
Umid92 <umidkerimov92@gmail.com>
2023-04-12 13:09:25 +0400

    POST route added

--- app.js
+++ app.js
@@ -5,8 +5,12 @@ const placesRoute = require("./routes/places-routes");
 
 const app = express();
 
+app.use(bodyparser.json());
 app.use("/api/places", placesRoute);
 app.use((error, req, res, next) => {
+  if (res.headerSent) {
+    return next(error);
+  }
   res.status(error.code || 500);
   res.json({ message: error.message || "Unknown error occured" });
 });
--- routes/places-routes.js
+++ routes/places-routes.js
@@ -3,6 +3,9 @@ const express = require("express");
 const placeConrollers = require("../controllers/places-controllers");
 
 const router = express.Router();
+
+router.post("/", placeConrollers.createPlace);
+
 router.get("/:pid", placeConrollers.getPlaceById);
 
 router.get("/users/:uid", placeConrollers.getPlaceByUserId);
